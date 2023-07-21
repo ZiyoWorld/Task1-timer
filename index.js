de24feaaccbb9f38f10a3f61574d1cc82b2c3cb1
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-07-21 15:14:47 +0500

    bag fixed

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ function timeBtn(){
 
 
 function updateCountDown(){
-    const hours = Math.floor((time / 3600) );
-    const minutes = Math.floor((time % 3600) / 60);
+    let hours = Math.floor((time / 3600) );
+    let minutes = Math.floor((time % 3600) / 60);
     let seconds = time % 60;
+    hours = hours < 10 ? "0" + hours : hours;
+    console.log(hours);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     countDown.innerHTML = `${hours}:${minutes}:${seconds}`;
     time--;
